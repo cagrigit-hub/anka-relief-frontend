981d224ea5709319bb30418726f73a9921ad585c
DogaOztuzun <unquale@gmail.com>
2023-02-08 20:14:50 +0300

    bilal

--- components/Team/Team.tsx
+++ components/Team/Team.tsx
@@ -80,7 +80,7 @@ function Team() {
                             </div>
                             <div className="p-6">
                                 <h5 className="text-lg font-bold mb-4">Eva Beylin</h5>
-                                <p className="text-gray-500 mb-4">Director @ Graph Foundation</p>
+                                <p className="text-gray-500 mb-4">Director @ The Graph Foundation</p>
                                 <ul className="list-inside flex mx-auto justify-center">
                                     <a href="https://twitter.com/evabeylin" className="px-2">
                                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className="w-4 h-4 text-blue-600">
@@ -227,6 +227,34 @@ function Team() {
                                 <p className="text-gray-500 mb-4">Co-founder @ Ukraine DAO</p>
                                 <ul className="list-inside flex mx-auto justify-center">
                                     <a href="https://twitter.com/cryptodrftng" className="px-2">
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className="w-4 h-4 text-blue-600">
+                                            <path fill="currentColor"
+                                                d="M459.37 151.716c.325 4.548.325 9.097.325 13.645 0 138.72-105.583 298.558-298.558 298.558-59.452 0-114.68-17.219-161.137-47.106 8.447.974 16.568 1.299 25.34 1.299 49.055 0 94.213-16.568 130.274-44.832-46.132-.975-84.792-31.188-98.112-72.772 6.498.974 12.995 1.624 19.818 1.624 9.421 0 18.843-1.3 27.614-3.573-48.081-9.747-84.143-51.98-84.143-102.985v-1.299c13.969 7.797 30.214 12.67 47.431 13.319-28.264-18.843-46.781-51.005-46.781-87.391 0-19.492 5.197-37.36 14.294-52.954 51.655 63.675 129.3 105.258 216.365 109.807-1.624-7.797-2.599-15.918-2.599-24.04 0-57.828 46.782-104.934 104.934-104.934 30.213 0 57.502 12.67 76.67 33.137 23.715-4.548 46.456-13.32 66.599-25.34-7.798 24.366-24.366 44.833-46.132 57.827 21.117-2.273 41.584-8.122 60.426-16.243-14.292 20.791-32.161 39.308-52.628 54.253z" />
+                                        </svg>
+                                    </a>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="mb-6 lg:mb-0">
+                        <div className="bg-white block rounded-lg shadow-lg">
+                            <div className="relative overflow-hidden bg-no-repeat bg-cover">
+                                <img src="team/bilal.png" className="w-full rounded-t-lg" />
+                                <a href="#!">
+                                    <div className="absolute top-0 right-0 bottom-0 left-0 w-full h-full overflow-hidden bg-fixed"></div>
+                                </a>
+                                <svg className="absolute team" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+                                    <path fill="#fff"
+                                        d="M0,288L48,272C96,256,192,224,288,197.3C384,171,480,149,576,165.3C672,181,768,235,864,250.7C960,267,1056,245,1152,250.7C1248,256,1344,288,1392,304L1440,320L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z">
+                                    </path>
+                                </svg>
+                            </div>
+                            <div className="p-6">
+                                <h5 className="text-lg font-bold mb-4">Bilal Bin Saqib</h5>
+                                <p className="text-gray-500 mb-4">Co-founder @ Tayaba & One Million Meals</p>
+                                <ul className="list-inside flex mx-auto justify-center">
+                                    <a href="https://twitter.com/Bilalbinsaqib" className="px-2">
                                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className="w-4 h-4 text-blue-600">
                                             <path fill="currentColor"
                                                 d="M459.37 151.716c.325 4.548.325 9.097.325 13.645 0 138.72-105.583 298.558-298.558 298.558-59.452 0-114.68-17.219-161.137-47.106 8.447.974 16.568 1.299 25.34 1.299 49.055 0 94.213-16.568 130.274-44.832-46.132-.975-84.792-31.188-98.112-72.772 6.498.974 12.995 1.624 19.818 1.624 9.421 0 18.843-1.3 27.614-3.573-48.081-9.747-84.143-51.98-84.143-102.985v-1.299c13.969 7.797 30.214 12.67 47.431 13.319-28.264-18.843-46.781-51.005-46.781-87.391 0-19.492 5.197-37.36 14.294-52.954 51.655 63.675 129.3 105.258 216.365 109.807-1.624-7.797-2.599-15.918-2.599-24.04 0-57.828 46.782-104.934 104.934-104.934 30.213 0 57.502 12.67 76.67 33.137 23.715-4.548 46.456-13.32 66.599-25.34-7.798 24.366-24.366 44.833-46.132 57.827 21.117-2.273 41.584-8.122 60.426-16.243-14.292 20.791-32.161 39.308-52.628 54.253z" />
