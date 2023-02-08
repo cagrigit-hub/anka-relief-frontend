d2440987ea4d41c3f901b93da94aefd257a5ed57
cagrigit-hub <cagrikutayok@hotmail.com>
2023-02-09 02:46:05 +0300

    responsive header

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -53,12 +53,14 @@ const isSticky = (e : any) => {
   const scrollTop = window.scrollY;
   if (scrollTop >= sidebarTop!) {
     sidebarEl?.classList.add('is-sticky');
+    sidebarEl?.classList.add('pl-24');
     hddnimg?.classList.add('appeal-img');
-    dntn?.classList.add('appeal-img');
+    dntn?.classList.add('appeal');
   } else {
     sidebarEl?.classList.remove('is-sticky');
+    sidebarEl?.classList.remove('pl-24');
     hddnimg?.classList.remove('appeal-img');
-    dntn?.classList.remove('appeal-img');
+    dntn?.classList.remove('appeal');
 
   }
 }
@@ -73,22 +75,25 @@ const isSticky = (e : any) => {
         <div className={classNames('text-gray-100 tracking-wider  leading-4 md:leading-[23px]  text-justify   text-[12px] md:text-[20px]  font-[300] w-[300px] md:w-[610px] ', roboto.className)}>
             <p>Crypto relief fund to support the people of Turkiye after disastrous earthquakes struck the region on February 6. Multi-sig operated by industry leaders, funding the best NGOs.</p>
         </div>
-        <a href="#donation"><button className='text-gray-100 text-base md:text-xl  border-solid mt-12 mb-24 border-[2px] px-6 pt-1 pb-2 md:px-10 md:pt-2 md:pb-4 rounded-full hover:bg-white  hover:text-black transition ease-in-out duration-300'>Donate</button></a>
-        <div id="sticky-top" className='mb-12 h-24 items-center relative bg-[#1F1F1F] flex w-full pl-[38%]'>
-            <img id="hddnimg" src="/svgs/anka-white.svg" className='w-12 left-16 absolute hidden'  alt="anka"/>
+        <a href="#donation"><button className='text-gray-100 text-base md:text-xl mb-12  border-solid mt-12  border-[2px] px-6 pt-1 pb-2 md:px-10 md:pt-2 md:pb-4 rounded-full hover:bg-white  hover:text-black transition ease-in-out duration-300'>Donate</button></a>
+        <div id="sticky-top" className='w-full pt-12  lg:pl-16 bg-[#1F1F1F]'>
+          <div className='mb-12  flex items-center justify-center  relative '>
+            <img id="hddnimg" src="/svgs/anka-white.svg" className='w-12 lg:left-16 absolute hidden'  alt="anka"/>
             {
             sections.map((section, index) => (
             
                 <a href={section.path}  key={index}>
-                <span className='text-[#d1cfcf] hover:text-white text-lg md:text-xl font-[400]  mr-12 md:mr-20'>{section.name}</span>
+                <span className='text-[#d1cfcf] hover:text-white text-[1.2rem] md:text-xl font-[400] mr-6 md:mr-12 lg:mr-20'>{section.name}</span>
                 </a>
             ))
             }
             <a id="dntn" className=" hidden " href={"#donation"}>
-            <span className='text-[#d1cfcf] hover:text-white text-lg md:text-xl font-[400]  mr-12 md:mr-20'>Donate</span>
+            <span className='text-[#d1cfcf] hover:text-white text-[1.2rem] md:text-xl font-[400]  mr-12 md:mr-20'>Donate</span>
 
             </a>
         </div>
+        </div>
+        
     </div>
   )
 }
